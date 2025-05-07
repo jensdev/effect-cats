e0315cbdbe80d8ba8b9e3b17bea04cd5606d03fc
jensdev <jens.verbeken@craftzing.com>
2025-05-07 13:07:06 +0200

    feat: add cats service

---
+++ src/services/cats.ts
@@ -0,0 +1,39 @@
+import { Context, Effect, HashMap, Layer, Ref } from "effect";
+import { Cat, CatId } from "../domain/cats";
+import { CatNotFoundError } from "../domain/errors";
+
+export class Cats extends Context.Tag("Cats")<
+  Cats,
+  {
+    readonly findById: (id: string) => Effect.Effect<Cat, CatNotFoundError>;
+    readonly persist: (cat: Cat) => Effect.Effect<string>;
+  }
+>() {}
+
+export const makeCats = Effect.gen(function* () {
+  const cats = yield* Ref.make(HashMap.empty<CatId, Cat>());
+
+  const findById = Effect.fn("Cats.findById")(function* (id: string) {
+    yield* Effect.log(`findById${id}`).pipe(Effect.annotateLogs({ id }));
+
+    return yield* Ref.get(cats).pipe(
+      Effect.flatMap(HashMap.get(id)),
+      Effect.catchTag(
+        "NoSuchElementException",
+        () => new CatNotFoundError({ id }),
+      ),
+    );
+  });
+
+  const persist = Effect.fn("Cats.persist")(function* (cat: Cat) {
+    yield* Effect.log(`persist cat ${cat}`).pipe(Effect.annotateLogs({ cat }));
+    return yield* Effect.succeed("Saved");
+  });
+
+  return {
+    findById,
+    persist,
+  } as const; // Using "as const" to keep the types readonly and strict
+});
+
+export const CatsLayer = Layer.effect(Cats, makeCats);
