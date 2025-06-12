53816b929468b712312ad361db382d0ae0c83d1e
jensdev <jens.verbeken@craftzing.com>
2025-06-12 15:31:11 +0200

    add swagger

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,5 +1,9 @@
 import { createServer } from "node:http";
-import { HttpApiBuilder, HttpMiddleware } from "@effect/platform";
+import {
+  HttpApiBuilder,
+  HttpApiSwagger,
+  HttpMiddleware,
+} from "@effect/platform";
 import { NodeHttpServer, NodeRuntime } from "@effect/platform-node";
 import { Layer } from "effect";
 import { ApiLive } from "./Api.js";
@@ -10,8 +14,8 @@ import { CatsServiceLive } from "./CatsService.js"; // Import CatsServiceLive
 const AppLive = Layer.provide(CatsServiceLive, CatsRepositoryLive);
 
 const HttpLive = HttpApiBuilder.serve(HttpMiddleware.logger).pipe(
+  Layer.provide(HttpApiSwagger.layer()),
   Layer.provide(ApiLive),
-  // Layer.provide(CatsRepositoryLive), // CatsRepositoryLive is now provided to CatsServiceLive
   Layer.provide(AppLive), // Provide the combined AppLive which includes CatsServiceLive
   Layer.provide(NodeHttpServer.layer(createServer, { port: 3000 })),
 );
