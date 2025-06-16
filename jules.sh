dc1f36a1b5ab84f0efa691f12f4063f2194b7426
jensdev <jens.verbeken@craftzing.com>
2025-06-16 13:45:31 +0200

    fix typo

--- jules.sh
+++ jules.sh
@@ -5,6 +5,6 @@ curl https://mise.run | sh
 ~/.local/bin/mise --version
 echo "eval \"\$(/home/swebot/.local/bin/mise activate bash)\"" >> ~/.bashrc
 
-cat ~/bashrc
+cat ~/.bashrc
 
 ~/.local/bin/mise use deno@latest
