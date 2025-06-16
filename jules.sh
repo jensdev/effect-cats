78047525f69b492edbbb80fc243c446f0e01d3c5
jensdev <jens.verbeken@craftzing.com>
2025-06-16 13:45:07 +0200

    retry

--- jules.sh
+++ jules.sh
@@ -4,5 +4,7 @@ curl https://mise.run | sh
 
 ~/.local/bin/mise --version
 echo "eval \"\$(/home/swebot/.local/bin/mise activate bash)\"" >> ~/.bashrc
-source ~/.bashrc
-mise use deno@latest
+
+cat ~/bashrc
+
+~/.local/bin/mise use deno@latest
